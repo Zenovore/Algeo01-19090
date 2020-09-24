5c48adfac67268ab61ef6cf37334c752b6bdbc88
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2020-09-24 18:47:36 +0700

    Clean main, change package name

--- src/Driver.java
+++ src/Driver.java
@@ -1,13 +1,8 @@
-package Matriks;
+package tubes;
 
 import java.io.*;
 class Driver{
   public static void main(String[] args){
-    Matriks m = new Matriks(3,3);
-    m.BacaMatriks();
-    System.out.printf("%.2f\n", m.Determinan());
-    m.Adjoin().TulisMatriks();
-    System.out.println("");
-    m.Invers().TulisMatriks();
+    /* TODO: Buat Main */
   }
 }
